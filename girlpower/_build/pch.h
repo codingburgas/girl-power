789081f191ffcdcd4a78152e47bd37442601c3bb
MVKoleva21 <MVKoleva21@codingburgas.bg>
2023-03-26 08:19:34 +1200

    Define colors

--- girlpower/_build/pch.h
+++ girlpower/_build/pch.h
@@ -6,9 +6,11 @@
 #include "frame.h"
 #include "loading.h"
 #include "main.h"
+
 #define DARKBLUE1 Color{ 3, 6, 23, 255 }
 #define DARKBLUE2 Color{ 7, 11, 38, 255 }
 #define DARKBLUE3 Color{ 12, 17, 48, 255 }
 #define PURPLE1 Color{ 38, 43, 71, 255 }
 #define PURPLE2 Color{ 57, 34, 238, 255 }
 #define BLUE1 Color{ 108, 123, 229, 255 }
+#define WHITE Color { 0, 0, 0, 255 }
