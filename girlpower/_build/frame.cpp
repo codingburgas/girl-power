5f849f78050d6c5f59c93e4d7a281eb9456555e3
MVKoleva21 <MVKoleva21@codingburgas.bg>
2023-03-26 08:11:39 +1200

    Add frame

---
+++ girlpower/_build/frame.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "frame.h"
+
+void printBubble(const int screenWidth, const int screenHeight)
+{
+		ClearBackground(DARKBLUE3);
+        Font Bubble = LoadFontEx("assets/fonts/Designer.ttf", 108, NULL, 0);
+        SetTextureFilter(Bubble.texture, TEXTURE_FILTER_BILINEAR);
+        DrawTextEx(Bubble, "BUBBLE", Vector2{ 300, 100 }, 96, 0.0f, RAYWHITE);
+        //raywhite should be switched to gradient class
+}
