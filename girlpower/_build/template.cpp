acc04e821bef87e9446e1d546cc035cf01bcedde
MVKoleva21 <MVKoleva21@codingburgas.bg>
2023-03-26 20:34:12 +0300

    Create a menu cipher layout template

--- girlpower/_build/template.cpp
+++ girlpower/_build/template.cpp
@@ -8,10 +8,45 @@ Rectangle layout =
 	1650,
 	850
 };
-
+Rectangle TopLeft =
+{
+	261,
+	213,
+	567,
+	266
+};
+Rectangle TopRight =
+{
+	915,
+	213,
+	567,
+	266
+};
+Rectangle BottomLeft =
+{
+	261,
+	552,
+	567,
+	266
+};
+Rectangle BottomRight =
+{
+	915,
+	552,
+	567,
+	266
+};
 void mainCipherTemplate(float roundness)
 {
 	ClearBackground(DARKBLUE3);
 
 	DrawRectangleRounded(layout, roundness, 10, DARKBLUE1);
+}
+void menuCipherTemplate(float roundness)
+{
+	ClearBackground(DARKBLUE3);
+	DrawRectangleRounded(TopLeft, roundness, 10, DARKBLUE1);
+	DrawRectangleRounded(TopRight, roundness, 10, DARKBLUE1);
+	DrawRectangleRounded(BottomLeft, roundness, 10, DARKBLUE1);
+	DrawRectangleRounded(BottomRight, roundness, 10, DARKBLUE1);
 }
