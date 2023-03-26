22ea1a8cb3309139b390f0f3b2b3d4c19b51027c
MVKoleva21 <MVKoleva21@codingburgas.bg>
2023-03-27 01:47:30 +0300

    Make loading page into function and style it

--- girlpower/_build/loading.cpp
+++ girlpower/_build/loading.cpp
@@ -1,74 +1,60 @@
-//#include "raylib.h"
-//#include "pch.h"
-//#include "loading.h"
-//
-//Rectangle loading =
-//{
-//        180,
-//        180,
-//        440,
-//        50
-//};
-//int main()
-//{
-//    const int screenWidth = 1720;
-//    const int screenHeight = 900;
-//
-//    InitWindow(screenWidth, screenHeight, "bubble");
-//
-//    InitAudioDevice();            
-//
-//    Music music = LoadMusicStream("assets/sounds/Loading.ogg");
-//
-//    PlayMusicStream(music);
-//
-//    float timePlayed = 0.0f;
-//    float roundness = 0.5f;
-//    bool pause = false;             
-//
-//    SetTargetFPS(30);         
-//
-//    while (!WindowShouldClose())    
-//    {
-//        UpdateMusicStream(music);   
-//
-//        if (IsKeyPressed(KEY_SPACE))
-//        {
-//            StopMusicStream(music);
-//            PlayMusicStream(music);
-//        }
-//
-//        if (IsKeyPressed(KEY_P))
-//        {
-//            pause = !pause;
-//
-//            if (pause) PauseMusicStream(music);
-//            else ResumeMusicStream(music);
-//        }
-//
-//        timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
-//
-//        if (timePlayed > 1.0f) timePlayed = 1.0f;   
-//
-//        BeginDrawing();
-//
-//        ClearBackground(DARKBLUE3);
-//
-//        DrawText("MUSIC SHOULD BE PLAYIN", 255, 150, 20, LIGHTGRAY);
-//
-//        DrawRectangleRounded(loading, roundness, 10, RAYWHITE);
-//        DrawRectangleRounded(Rectangle{ 200, 200, (float)(timePlayed * 400.0f), 12 }, roundness, 10, PURPLE2);
-//
-//        DrawText("space - restart audio", 215, 250, 20, LIGHTGRAY);
-//        DrawText("p - pause", 208, 280, 20, LIGHTGRAY);
-//
-//        EndDrawing();
-//        
-//    }
-//    UnloadMusicStream(music);   
-//
-//    CloseAudioDevice();        
-//
-//    CloseWindow();         
-//}
-////align needed
+#include "pch.h"
+
+Rectangle loading =
+{
+        285,
+        372,
+        1157,
+        156
+};
+bool LoadingLoop(float timePlayed, float roundness)
+{
+    InitAudioDevice();            
+
+    Music music = LoadMusicStream("assets/sounds/Loading.ogg");
+
+    PlayMusicStream(music);
+    bool pause = false;             
+
+    SetTargetFPS(30);         
+
+    while (!WindowShouldClose())    
+    {
+        UpdateMusicStream(music);   
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            StopMusicStream(music);
+            PlayMusicStream(music);
+        }
+
+        if (IsKeyPressed(KEY_P))
+        {
+            pause = !pause;
+
+            if (pause) PauseMusicStream(music);
+            else ResumeMusicStream(music);
+        }
+
+        timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
+
+        if (timePlayed > 1.0f) timePlayed = 1.0f;   
+
+        BeginDrawing();
+        float round = 0.1f;
+        mainCipherTemplate(round);
+        DrawText("LOADING...", 739, 240, 60, LIGHTGRAY);
+        DrawRectangleRounded(loading, roundness, 10, RAYWHITE);
+        DrawRectangleRounded(Rectangle{ 401, 430, (float)(timePlayed * 850.0f), 50 }, roundness, 50, PURPLE2);
+
+        EndDrawing();
+        
+    }
+    UnloadMusicStream(music);   
+
+    CloseAudioDevice();        
+
+    CloseWindow();         
+
+    return false;
+}
